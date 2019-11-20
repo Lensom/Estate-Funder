2d621bfcf739189aafb7f472d379ff5b14e20cce
Lensom <magurawork@gmail.com>
2019-11-20 17:16:51 +0200

    7.25. Chart js and home page is done

--- project/js/common.js
+++ project/js/common.js
@@ -1,27 +1,153 @@
 // Mobile menu
 $(document).ready(function () {
-    $('.header__mobile').on('click', function() {
+    $('.header__mobile').on('click', function () {
         $('body').toggleClass('show');
     })
 });
 
 
 // Owl carousel
-$(document).ready(function(){
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         items: 2,
         margin: 20,
+        nav: true,
+        dost: false,
         responsive: {
             768: {
                 margin: 40,
                 items: 3
             },
             568: {
-                items: 2
+                items: 2,
+                loop: true,
+                center: true,
+                autoWidth: false
             },
             320: {
-                items: 1
+                items: 1,
+                autoWidth: true,
+                center: true,
+                loop: true
             }
         }
     });
+});
+
+
+// Chart.js Main page
+$(document).ready(function () {
+
+    var ctx = document.getElementById('myChart').getContext("2d");
+
+    var gradientStroke = ctx.createLinearGradient(100, 0, 500, 0);
+    gradientStroke.addColorStop(0, '#3f89e8');
+    gradientStroke.addColorStop(1, '#5ebafe');
+
+    var gradientFill = ctx.createLinearGradient(0, 200, 0, 0);
+    gradientFill.addColorStop(0, "rgba(115, 189, 245, 0)");
+    gradientFill.addColorStop(1, "rgba(63, 137, 232, 1)");
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["", "Year 1", "Year 2", "Year 3", ""],
+            datasets: [{
+                label: "Approx",
+                borderColor: gradientStroke,
+                pointBorderColor: '#fff',
+                pointBackgroundColor: gradientStroke,
+                pointHoverBackgroundColor: gradientStroke,
+                pointHoverBorderColor: gradientStroke,
+                pointBorderWidth: 5,
+                pointHoverRadius: 7,
+                pointHoverBorderWidth: 1,
+                pointRadius: 10,
+                fill: true,
+                backgroundColor: gradientFill,
+                borderWidth: 4,
+                data: [20000, 23000, 32000, 35387, 40000],
+                lineTension: 0
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        fontColor: "#475871",
+                        fontStyle: "500",
+                        fontFamily: "AvenirNextCyr",
+                        fontSize: 14,
+                        beginAtZero: false,
+                        min: 10000,
+                        maxTicksLimit: 5,
+                        padding: 20,
+                    },
+                    gridLines: {
+                        drawTicks: false,
+                        display: false
+                    }
+
+                }],
+                xAxes: [{
+                    gridLines: {
+                        zeroLineColor: "transparent"
+                    },
+                    ticks: {
+                        fontColor: "#475871",
+                        fontStyle: "500",
+                        fontFamily: "AvenirNextCyr",
+                        fontSize: 14,
+                        beginAtZero: false,
+                        min: 10000,
+                        maxTicksLimit: 5,
+                        padding: 20,
+                    }
+                }]
+            },
+            tooltips: {
+                backgroundColor: '#fff',
+                titleFontColor: '#475871',
+                titleAlign: 'center',
+                titleFontFamily: "AvenirNextCyr",
+                titleFontStyle: "500",
+                titleFontSize: 14,
+                bodyFontColor: '#1a273a',
+                bodyFontSize: 18,
+                bodyFontFamily: "AvenirNextCyr",
+                bodyFontStyle: "700",
+                displayColors: false,
+                cornerRadius: 10,
+                xPadding: 10,
+                yPadding: 15,
+                callbacks: {
+                    title: function (tooltipItem, data) {
+                        return 'Approx'
+                    },
+                    label: function (tooltipItem, data) {
+                        var value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].toLocaleString();
+                        console.log(value)
+                        return value;
+                    }
+                }
+            }
+        }
+    });
+
+    let lengthData = myChart.data.datasets[0].data.length;
+    let radius = myChart.data.datasets[0].pointRadius;
+    let radiusArray = [];
+    for (let i = 0; i < lengthData; i++) {
+        radiusArray.push(radius);
+    }
+
+    radiusArray[0] = 0;
+    radiusArray[radiusArray.length - 1] = 0;
+
+    myChart.data.datasets[0].pointRadius = radiusArray;
+    myChart.update();
+
 });
